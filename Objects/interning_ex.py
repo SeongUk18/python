9fca8fd4c6d6ca9a68f2f50295073b7d23a3d697
SeongUk18 <adulty22@gmail.com>
2024-05-13 17:42:25 +0900

    Feat: Object study
    interning

---
+++ Objects/interning_ex.py
@@ -0,0 +1,14 @@
+# 정수 인터닝
+a = 256
+b = 256
+print(a is b)  # True - 같은 객체
+
+a = 257
+b = 257
+print(a is b)  # 가능성에 따라 True 또는 False
+
+# 문자열 인터닝
+a = "hello"
+b = "hello"
+print(a is b)  # True
+
