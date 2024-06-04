f2bd5e26ee6ba8b40a2be815d84d6e905ed58a8e
SeongUk18 <adulty22@gmail.com>
2024-06-04 09:26:31 +0900

    Feat: iterator 예제

---
+++ 14.Generator/iterator_ex.py
@@ -0,0 +1,28 @@
+class MyIterator:
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.data):
+            result = self.data[self.index]
+            self.index += 1
+            return result
+        else:
+            raise StopIteration
+
+
+my_iter = MyIterator([1, 2, 3, 4, 5])
+for item in my_iter:
+    print(item)
+
+'''
+1
+2
+3
+4
+5
+'''
