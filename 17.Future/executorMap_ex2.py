13f16949091971848188c0b2b7dc0bfeebe9f50b
SeongUk18 <adulty22@gmail.com>
2024-06-09 15:36:37 +0900

    Feat: Executor.map 예제 구현

---
+++ 17.Future/executorMap_ex2.py
@@ -0,0 +1,23 @@
+import concurrent.futures
+
+
+def compute_square(n):
+    return n * n
+
+
+numbers = [1, 2, 3, 4, 5]
+
+if __name__ == '__main__':
+    with concurrent.futures.ProcessPoolExecutor(max_workers=3) as executor:
+        results = executor.map(compute_square, numbers)
+
+        for result in results:
+            print(result)
+
+'''
+1
+4
+9
+16
+25
+'''
